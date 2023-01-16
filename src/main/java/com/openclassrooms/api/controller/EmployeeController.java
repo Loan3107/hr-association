10218b2174d5a5c2cf84db72bcc5bcd07adff927
PIROTAIS <loan.pirotais@cgi.com>
2023-01-16 10:49:55 +0100

    core(api): enabling cors

--- src/main/java/com/openclassrooms/api/controller/EmployeeController.java
+++ src/main/java/com/openclassrooms/api/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.openclassrooms.api.model.Employee;
 import com.openclassrooms.api.service.EmployeeService;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class EmployeeController {
 	@Autowired
