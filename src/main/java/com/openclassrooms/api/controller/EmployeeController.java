4d705f4e298176dce326d046f55951f1bb64e39a
PIROTAIS <loan.pirotais@cgi.com>
2023-01-17 09:21:40 +0100

    feat(employees): implementing put endpoint

--- src/main/java/com/openclassrooms/api/controller/EmployeeController.java
+++ src/main/java/com/openclassrooms/api/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,18 @@ public class EmployeeController {
 	@ResponseStatus(value = HttpStatus.CREATED, code = HttpStatus.CREATED)
 	public Employee create(@RequestBody Employee employee) {
 		return employeeService.saveEmployee(employee);
+	}
+	
+	/**
+	 * PUT - Employee
+	 * @param id - Id of the employee
+	 * @param employee - Employee with modified informations
+	 * @return - The updated Employee
+	 */
+	@PutMapping("/employee/{id}")
+	@ResponseStatus(value = HttpStatus.OK, code = HttpStatus.OK)
+	public Employee put(@PathVariable("id") final Long id, @RequestBody Employee employee) {
+		return employeeService.updateEmployee(id, employee);
 	}
 	
 	/**
